caf1373971b0cb46e6d824975ec31866556dbb6f
Sakyi Mustapha <sakyimustapha5@gmail.com>
2025-06-17 19:39:49 +0000

    Added a databade and the authenticaion

--- app.py
+++ app.py
@@ -1,18 +1,59 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return '<h1>Hello Mindspace</h1>'
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mindspace.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)    
+    firstname = db.Column(db.String(50), nullable=False)
+    lastname = db.Column(db.String(50),  nullable=False)
+    email = db.Column(db.String(150), unique=True,  nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = register.form
+    firstname = data.get('fname')
+    lastname = data.get('lname')
+    email = data.get('email')
+    password = data.get('password')
+
+    
+    
+    if not (firstname and lastname and email and password):
+        return jsonify({'error': 'all fields must be field'}),400
+    
+    #hash the password, making it invisible
+    hashed_password = generate_password_hash(password)
 
 
 
-@app.route('/me')
-def out():
-    return '<h1>Authentication</h1>'
+    #saves user info in the database
+    new_user = User(firstname=firstname, lastname=lastname, email=email, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+     
+    new_user = User(firstname=firstname, lastname=lastname, email=email, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+
+
+
 
 if __name__ == '__main__':
+    #this allows sqlalchemy to find data and create data according to your db.Model
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
 
 
