b64c74bef4174a9d65a5e1bc70e89db1353143d0
Sakyi Mustapha <sakyimustapha5@gmail.com>
2025-07-15 01:23:30 +0000

    added a jwt and fixed some bugs

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request,  jsonify
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token, set_access_cookies
 import os
 
 
@@ -14,33 +15,23 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)    
-    firstname = db.Column(db.String(50), nullable=False)
-    lastname = db.Column(db.String(50),  nullable=False)
-    email = db.Column(db.String(150), unique=True,  nullable=False)
-    password = db.Column(db.String(150), nullable=False)
+    firstname = db.Column(db.String(50),)
+    lastname = db.Column(db.String(50),)
+    email = db.Column(db.String(150), unique=True,)
+    password = db.Column(db.String(150),)
 
 
-@app.route('/register', methods=['POST'])
+@app.route('/signup', methods=['POST'])
 def register():
     
     #this only accept a json not a form
     data = request.get_json()
-    
     firstname = data.get('fname')
     lastname = data.get('lname')
     email = data.get('email')
     password = data.get('password')
 
-    #this checks if the user filled all sections and if yes the user logs in successfully
-    if (firstname and lastname and email and password):
-        return jsonify({'message': 'user registered successfully'}), 201
-    
-    
-    #checks if all fields are not field, if not throws the bellow error
-    if not any ([firstname, lastname, email, password]):
-        return jsonify({'message': 'Empty fields, fill up the fields'}), 400
 
-    
     #stores empty fields then through an error of each
     missing_fields = []
 
@@ -55,30 +46,68 @@ def register():
 
     if missing_fields:
         return jsonify({'error': f'Missing fields: {", ".join(missing_fields)}'}), 400
-
+    
     
 
-    #Check if email already exists
+#Check if email already exists
     existing_user = User.query.filter_by(email=email).first()
     if existing_user:
         return jsonify({"error": "Email already registered"}), 409
-    
-    
     
     #hash the password, making it invisible
     hashed_password = generate_password_hash(password)
 
 
 
-    #saves user info in the database
+   #saves user info in the database
     new_user = User(firstname=firstname, lastname=lastname, email=email, password=hashed_password)
     db.session.add(new_user)
     db.session.commit()
+    return jsonify({'message': 'Account created successfully'}), 201
      
-    new_user = User(firstname=firstname, lastname=lastname, email=email, password=hashed_password)
-    db.session.add(new_user)
-    db.session.commit()
 
+
+
+#logged in code
+@app.route('/login', methods=['POST'])
+def login():
+    
+    data = request.get_json()
+    firstname = request.get_data('fname')
+    lastname = request.get_data('lname')
+    email = request.get_data('email')
+    password = request.get_data('password')
+
+        
+    Missing_fields = []
+    
+    if firstname not in db:
+            Missing_fields.append('firstname')
+    if lastname not in db:
+            Missing_fields.append('lastname')
+    if email not in db:
+            Missing_fields.append('email')
+    if password not in db:
+            Missing_fields.append('password')
+
+    if Missing_fields:
+             jsonify({"Error": f"Missing_fields: {Missing_fields}"}), 400
+        
+    
+    
+    
+    #find a user by email 
+    existing_user = User.query.filter_by(email=email).first()
+
+    if not existing_user:
+        return jsonify({'message': 'User not found'}), 400
+        
+    
+    response = jsonify({'msg': 'logged in successfully'})
+    #create an access token for the user to verify their identity when visiting a protected route
+    access_token = create_access_token(identity=email)
+    set_access_cookies(response, access_token)
+    return response
 
 
 
